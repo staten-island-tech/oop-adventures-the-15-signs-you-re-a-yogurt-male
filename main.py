4bfe36b277559bd876480aaf6724728a5493555b
blueeejay <jadec153@nycstudents.net>
2026-05-08 10:08:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ character_list = ["fulgrim", "house","whalen", "jon","wizard","kevin","cecil","s
 def initialize():
     Body_Room = random.choice(Murderedrooms_list)
     Murderer = random.choice(character_list)
-    print(Murderer)
 
     intro_writing = ["You awake from your shallow rest with a start. ",
     "It took a few moments for you to realize where you were: you're not used to sleeping in a supply closet.",
     "To be fair, you weren’t meant to be in a supply closet at all. You barricaded yourself in here so you could rest safely.",
      "March 13th marked start date of the Discovery program-- Staten Island Tech’s Discovery program-- ",
-     "and that day, you and 6 of your peers were to study in order to claim one of their additional seats.",
+     "and that day, you and 9 of your peers were to study in order to claim one of their additional seats.",
      "~~~~",
     "All was well until the second day of the program, and a tumultuous summer storm arrived.",
     "Initially, only two of your teachers had called out, but now all of them were gone.",
