20ef39d765ba0ee1914988ffe29c73f18593e23a
blueeejay <jadec153@nycstudents.net>
2026-05-08 10:00:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # welcome 
 import random
+# first murderer EVER generated: mr. whalen 
 Murderedrooms_list = ["Cafeteria","Compsci Lab", "Goyco Room", "Gymnasium", "Library", "Henriques Engineering", "Secret pool on the roof","Auditorium" ]
 character_list = ["fulgrim", "house","whalen", "jon","wizard","kevin","cecil","sydney"]
 
