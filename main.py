c500cb077d539914bd10a8aa260cd7816ba6f729
blueeejay <jadec153@nycstudents.net>
2026-05-08 09:59:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# welcome 
+# hallway goal: select list of names.. ect
+def movethru():
+    rooms_list = ["Cafeteria","Compsci Lab", "Goyco Room", "Gymnasium", "Library", "Henriques Engineering", "Secret pool on the roof","Auditorium"]
+    for index, x in enumerate(rooms_list):
+            print(index, ":", x)
+movethru()
