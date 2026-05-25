bff3ff20193dc4b0bac534bf57324cf8e4bb4890
screaming in agony <ameliar51@nycstudents.net>
2026-05-25 14:51:34 -0400

    5/25

--- cackling.py
+++ cackling.py
@@ -1,38 +1,5 @@
 import evil
 
-""" initial = ["How have you been holding up?",
-           "Hey! Have you seen anything lately?",
-           "Are you the one who killed Julius??",
-           "[search character]"]
-#How have you been holding up?
-initial_1 = ["It sure is unfortunate that Julius has died...",
-             "Hey so. Did you kill Julius",
-             "Have you seen anything unusual lately perhaps?"
-             "[Search character]"]
-#Hey! Have you seen anything lately?
-initial_2 = ["Okay, thank you!",
-             "Goodbye",
-             "[search character]"]
-#Are you the one who killed Julius??
-initial_3 = ["Well who did then?", 
-             "Okay. Have you seen anything?",
-             "I don't believe you", 
-             "Goodbye",
-             ""]
-#[Search character]
-initial_4 = ["Yes", "No"]
-
-#It sure is unfortunate that Julius has died
-unfortunate_1 = ["Have you seen anything unusual lately perhaps?", 
-             "Okay, thank you!", 
-             "Goodbye", 
-             "[search character]"]
-#I don't believe you
-accusation_1 = [">:(", 
-                "Goodbye",
-                "[search character]"] """
-
-#dictionary version of the lists above
 dialogues = [
     {"q" : "initial",
      "qs": ["How have you been holding up?",
@@ -43,7 +10,7 @@ dialogues = [
     {"q" :"initial_1" ,
      "qs": ["It sure is unfortunate that Julius has died...",
             "Hey so. Did you kill Julius",
-            "Have you seen anything unusual lately perhaps?"
+            "Have you seen anything unusual lately perhaps?",
             "[Search character]"]},
     #Hey! Have you seen anything lately?
     {"q" :"initial_2" ,
@@ -63,7 +30,7 @@ dialogues = [
     #It sure is unfortunate that Julius has died
     {"q" :"unfortunate_1" ,
      "qs": ["Have you seen anything unusual lately perhaps?", 
-            "Okay, thank you!", 
+            "Hey so. Did you kill Julius", 
             "Goodbye", 
             "[search character]"]},
     #I don't believe you
@@ -78,24 +45,72 @@ c_dialogues = [
      "intro": "\n15 March, undisclosed year, regarding the death of Julius Caesar. \nStatement begins...\n"
          "It is not often that I find myself facing the murder of a classmate within a school building,\n-----",
      "initial_1" : "\nI am fine, but it does not seem that everyone else appears to be as well.\n-----",
-     "initial_2" : "You certainly have some nerve to interrupt an archival recording. Make it quick-\n"
-         "I do believe I may have seen {person} carrying {weapon} around… there was a strange sound coming "
-         "from {room} as well but I cannot be sure.",
+     "initial_2" : "You certainly have some nerve to interrupt an archival recording.\n"
+         "I do believe I may have seen {person} carrying {weapon} around... and there was a strange sound coming "
+         "from {room} as well, but I cannot be sure.",
      "initial_3" : "Excuse me? What makes you think I would do such a thing as to *murder* a peer of mine, however much I may dislike them?",
      "initial_4" : "Are you sure you want to search this character? You will gain no useful information from this action.",
-     "unfortunate_1": "I was walking past {room} earlier and did see {person} with {weapon}, but I am unsure.",
+     "unfortunate_1": "Indeed.",
+     "thanks" : "[watches you leave]",
+     "goodbye" : "Good riddance. please do not return."
      }
 ]             
 
-
-def interact(character):
-    print(c_dialogues[character]["intro"])
-    for index, option in enumerate(dialogues[0]["qs"]):
+def d_setup(num):
+    for index, option in enumerate(dialogues[num]["qs"]):
         print(index,":", option)
+def response(char, resp):
+    print(c_dialogues[char][resp])
+
+def initial_1(char):
     select = int(input("Select a dialogue option:  "))
     if select == 0:
-        print(c_dialogues[character]["initial_1"])
-        for index, option in enumerate(dialogues[1]["qs"]):
-            print(index,":", option)
+        response(char, "unfortunate_1")
+        d_setup(5)
+    if select == 1:
+        response(char, "initial_3")
+        d_setup(3)
+    if select == 2:
+        response(char, "initial_2")
+        d_setup(2)
+    if select == 3:
+        response(char, "initial_4")
+        d_setup(3)
+
+def initial_2(char):
+    select = int(input("Select a dialogue option:  "))
+    if select == 0:
+        response(char, "thanks")
+    if select == 1:
+        response(char, "goodbye")
+    if select == 2:
+        response(char, "initial_2")
+        d_setup(4)
+
+def qround_1(char):
+    select = int(input("Select a dialogue option:  "))
+    if select == 0:
+        response(char, "initial_1")
+        d_setup(1)
+        initial_1(char)
+    elif select == 1:
+        response(char, "initial_2")
+        d_setup(2)
+
+    elif select == 2:
+        response(char, "initial_3")
+        d_setup(3)
+
+    elif select == 3:
+        response(char, "initial_4")
+        d_setup(4)
+
+    else:
+        print("Dialogue option does not exist. Please choose again")
+
+def interact(character):
+    response(character, "intro")
+    d_setup(0)
+    qround_1(character)
 
 interact(0)
